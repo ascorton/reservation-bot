d5601a1dd77e724142e8a26732fd0e0001085d70
Alex <alejandro.scorton@gmail.com>
2026-07-27 18:03:50 +0200

    Prepared script to be launched automatically

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from booking import run_booking_script
+import time
+import schedule
 
-if __name__ == "__main__":
+def run_job():
+    print("Executing class booking")
     run_booking_script()
+
+schedule.every().day.at("16:00").do(run_job)
+
+print("Booking bot initiated. Awaiting 16:00...")
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
 
