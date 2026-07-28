2d9bec609ea94a2f1d6e9ed1105331fa8c4cdff0
Alex <alejandro.scorton@gmail.com>
2026-07-28 17:06:40 +0200

    Automated to run continously

--- main.py
+++ main.py
@@ -6,7 +6,19 @@ def run_job():
     print("Executing class booking")
     run_booking_script()
 
-schedule.every().day.at("16:00").do(run_job)
+schedule.every().sunday.at("16:00").do(run_job)
+schedule.every().sunday.at("17:00").do(run_job)
+
+schedule.every().monday.at("16:00").do(run_job)
+schedule.every().monday.at("17:00").do(run_job)
+
+schedule.every().tuesday.at("16:00").do(run_job)
+schedule.every().tuesday.at("17:00").do(run_job)
+
+schedule.every().thursday.at("16:00").do(run_job)
+schedule.every().thursday.at("17:00").do(run_job)
+
+schedule.every().friday.at("09:00").do(run_job)
 
 print("Booking bot initiated. Awaiting 16:00...")
 
